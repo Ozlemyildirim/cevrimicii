7d80bc384099695b26c7b8aaa2e72236dfd2d3d9
semaruken <semaruken@gmail.com>
2016-04-23 21:29:04 +0300

    sirket

---
+++ src/main/java/com/project/entity/Sirket.java
@@ -0,0 +1,103 @@
+package com.project.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "SIRKET")
+public class Sirket extends BaseEntity {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private String adi;
+	private String sektoru;
+	private int calisansayisi;
+	private String adresi;
+	private String ülke;
+	private String sehir;
+	private String webadresi;
+
+	@Id
+	@SequenceGenerator(name = "SEQ_SIRKET_ID", sequenceName = "SEQ_SIRKET_ID", allocationSize = 1, initialValue = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_SIRKET_ID")
+	@Column(name = "ID")
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Column(name = "SIRKETADI")
+	public String getAdi() {
+		return adi;
+	}
+
+	public void setAdi(String adi) {
+		this.adi = adi;
+	}
+
+	@Column(name = "SEKTORU")
+	public String getSektoru() {
+		return sektoru;
+	}
+
+	public void setSektoru(String sektoru) {
+		this.sektoru = sektoru;
+	}
+
+	@Column(name = "CALISANSYISI")
+	public int getCalisansayisi() {
+		return calisansayisi;
+	}
+
+	public void setCalisansayisi(int calisansayisi) {
+		this.calisansayisi = calisansayisi;
+	}
+
+	@Column(name = "SIRKETADRESI")
+	public String getAdresi() {
+		return adresi;
+	}
+
+	public void setAdresi(String adresi) {
+		this.adresi = adresi;
+	}
+
+	@Column(name = "ULKE")
+	public String getÜlke() {
+		return ülke;
+	}
+
+	public void setÜlke(String ülke) {
+		this.ülke = ülke;
+	}
+
+	@Column(name = "SIRKETSEHRI")
+	public String getSehir() {
+		return sehir;
+	}
+
+	public void setSehir(String sehir) {
+		this.sehir = sehir;
+	}
+
+	@Column(name = "WEBADRESI")
+	public String getWebadresi() {
+		return webadresi;
+	}
+
+	public void setWebadresi(String webadresi) {
+		this.webadresi = webadresi;
+	}
+
+}
---
+++ src/main/java/com/project/mbeans/SirketBean.java
@@ -0,0 +1,93 @@
+package com.project.mbeans;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import com.project.entity.Sirket;
+import com.project.service.SirketService;
+
+@Controller("SirketBean")
+@Scope("view")
+public class SirketBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	/**
+	 * 
+	 */
+
+	@Autowired
+	private transient SirketService SirketService;
+
+	private Sirket Sirket;
+	private String mesaj;
+	List<Sirket> SirketList;
+
+	@PostConstruct
+	public void init() {
+		SirketList = SirketService.getAll();
+	}
+
+	public void kaydet() {
+		System.out.println(Sirket.toString());
+
+		if (this.Sirket != null && this.Sirket.getId() != null) {
+			SirketService.update(Sirket);
+		} else {
+			SirketService.save(Sirket);
+		}
+
+		SirketList = SirketService.getAll();
+
+		FacesContext.getCurrentInstance().addMessage("Kayýt",
+				new FacesMessage("Sirket Kaydedildi"));
+		Sirket = new Sirket();
+	}
+
+	public void sil(Long id) {
+		Sirket entity = SirketService.getById(id);
+		SirketService.delete(entity);
+		SirketList = SirketService.getAll();
+		FacesContext.getCurrentInstance().addMessage("Kayýt",
+				new FacesMessage("Sirket Silindi"));
+	}
+
+	public void duzenle(Long id) {
+		Sirket entity = SirketService.getById(id);
+		this.Sirket = entity;
+	}
+
+	public void yeni() {
+		this.Sirket = new Sirket();
+	}
+
+	public Sirket getSirket() {
+		if (Sirket == null) {
+			Sirket = new Sirket();
+		}
+		return Sirket;
+	}
+
+	public void setSirket(Sirket Sirket) {
+		this.Sirket = Sirket;
+	}
+
+	public String getMesaj() {
+		return mesaj;
+	}
+
+	public List<Sirket> getSirketList() {
+		return SirketList;
+	}
+
+}
